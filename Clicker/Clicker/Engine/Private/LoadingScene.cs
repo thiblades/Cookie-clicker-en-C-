505e20cb91a6fb925d79d82cf479c097dca19be3
deadc0der <jessy.vanderaugstraete@gmail.com>
2017-10-27 13:12:40 +0200

    Add logo and progress bar to loading screen

--- Clicker/Clicker/Engine/Private/LoadingScene.cs
+++ Clicker/Clicker/Engine/Private/LoadingScene.cs
@@ -11,6 +11,11 @@ namespace Clicker.Engine.Private {
         private float progress = 0;
 
         private Text loadingText = null;
+        private Sprite logoSprite = null;
+        private RectangleShape progressBar = null;
+        private float progressBarWidth;
+
+        public static int PROGRESS_BAR_HEIGHT = 12;
 
         public LoadingScene() {
         }
@@ -30,30 +35,71 @@ namespace Clicker.Engine.Private {
         }
 
         override public void Load(IProgressReport pr){
-            // Just load the font
+            // Load the font we'll use for the logo
             loadingText = new Text();
             loadingText.Font = new Font("Assets/GenericFont.otf");
-            loadingText.Color = Color.White;
+            loadingText.Color = Color.Black;
             loadingText.DisplayedString = "";
-            loadingText.CharacterSize = 60;
+            loadingText.CharacterSize = 24;
+
+            // Load the logo we'll be displaying
+            Texture texture = new Texture("Assets/Logo.png");
+            texture.Repeated = false;
+            texture.Smooth = true;
+            logoSprite = new Sprite(texture);
+
+            // Create the rectangle shape we need
+            progressBar = new RectangleShape();
+            progressBar.FillColor = Color.Black;
+            progressBar.Size = new Vector2f(0, PROGRESS_BAR_HEIGHT);
         }
 
         override public void Layout(Vector2u newSize){
-            // Center the status text.
+            float TargetLogoSize = newSize.Y / 2.0f;
+
+            // Scale and center the logo
+            logoSprite.Scale = new Vector2f(
+                TargetLogoSize / logoSprite.Texture.Size.X,
+                TargetLogoSize / logoSprite.Texture.Size.Y
+            );
+
+            logoSprite.Position = new Vector2f(
+                (newSize.X - TargetLogoSize) / 2,
+                (newSize.Y - TargetLogoSize) / 2
+            );
+
+            // Now place the text right below that
             FloatRect bounds = loadingText.GetLocalBounds();
 
             loadingText.Position = new Vector2f(
+                // Center horizontally
                 (newSize.X - bounds.Width) / 2,
-                (newSize.Y - bounds.Height) / 2
+
+                // Place right under the logo vertically
+                logoSprite.Position.Y + TargetLogoSize + bounds.Height
             );
+
+            // Finally, place the progress bar right under this
+            progressBarWidth = TargetLogoSize * 1.5f;
+
+            progressBar.Size = new Vector2f(progressBarWidth, PROGRESS_BAR_HEIGHT);
+
+            progressBar.Position = new Vector2f(
+                (newSize.X - progressBarWidth) / 2,
+                loadingText.Position.Y + bounds.Height + 12
+            );
+
+            progressBar.Scale = new Vector2f(progress, 1);
         }
 
         override public void Update(float dt){
         }
 
         override public void Render(RenderTarget target){
-            target.Clear(Color.Black);
+            target.Clear(Color.White);
+            target.Draw(logoSprite);
             target.Draw(loadingText);
+            target.Draw(progressBar);
         }
     }
 }
