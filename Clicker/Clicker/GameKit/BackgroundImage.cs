58374bd8df31dc3a500725faed8113eff85ce661
deadc0der <jessy.vanderaugstraete@gmail.com>
2017-11-20 16:55:49 +0100

    GameKit: Add generic "menu" scene + helpers used in building it

---
+++ Clicker/Clicker/GameKit/BackgroundImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Clicker.GameKit {
+    public class BackgroundImage {
+        public string imagePath;
+
+        private Texture texture;
+        private Sprite sprite;
+
+        public BackgroundImage(string path) {
+            imagePath = path;
+
+            texture = new Texture(path);
+            sprite = new Sprite(texture);
+            sprite.Position = new Vector2f(0, 0);
+        }
+
+        public void Layout(Vector2u newSize){
+            sprite.Scale = new Vector2f(
+                newSize.X / (float) texture.Size.X,
+                newSize.Y / (float) texture.Size.Y
+            );
+        }
+
+        public void Render(RenderTarget rt){
+            rt.Draw(sprite);
+        }
+    }
+}
