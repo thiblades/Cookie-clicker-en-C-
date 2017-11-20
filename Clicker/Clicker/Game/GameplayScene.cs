9e3590e21370af6aa0b98f6e3f36f0c1bff49e19
deadc0der <jessy.vanderaugstraete@gmail.com>
2017-11-20 23:40:40 +0100

    Add main game scene
    (cherry picked from commit 3ac8833954ab18acb14d236f08b2f6646bab09d9)

---
+++ Clicker/Clicker/Game/GameplayScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Clicker.Engine.Public;
+using Clicker.GameKit;
+
+using SFML.System;
+using SFML.Graphics;
+
+namespace Clicker.Game {
+    public class GameplayScene: Scene {
+        public const string BACKGROUND_IMAGE = "Assets/Background.png";
+
+        private BackgroundImage background;
+        private TimeAccumulator time;
+
+        public GameplayScene() {
+        }
+
+        public override void Load(IProgressReport pr){
+            // Load all images
+            pr.ReportProgress(0, "Chargement des images...");
+            background = new BackgroundImage(BACKGROUND_IMAGE);
+
+            // Prpeare the time accumulator
+            time = new TimeAccumulator();
+        }
+
+        public override void Layout(Vector2u newSize){
+            background.Layout(newSize);
+        }
+
+        public override void Update(float dt){
+            time.Frame(dt);
+        }
+
+        public override void Render(RenderTarget rt){
+            background.Render(rt);
+        }
+    }
+}
